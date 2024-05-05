f48e56ed680d3a9f9063374e4a5751acd67e6d02
Mostary Jahan <mostaryjahan01@gmail.com>
2024-05-05 14:42:12 +0600

    added patch

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 
@@ -28,13 +28,85 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+   const serviceCollection = client.db('carDoctor').collection('services');
+   const bookingCollection = client.db('carDoctor').collection('booking');
+
+   app.get('/services', async(req, res) =>{
+     const cursor = serviceCollection.find();
+     const result = await cursor.toArray();
+     res.send(result);
+   })
+
+
+   //for service id
+   app.get('/services/:id', async(req, res) =>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)};
+    const options ={
+        projection : { title: 1, price: 1, service_id: 1, img: 1},
+    }
+    const result = await serviceCollection.findOne(query, options);
+    res.send(result);
+  })
+
+
+  //booking
+  
+  app.get('/bookings', async(req, res) =>{
+    console.log(req.query.email);
+    let query ={};
+    if(req.query?.email){
+        query= {email: req.query.email}
+    }
+    const cursor = bookingCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+  })
+
+  app.post('/bookings', async(req, res) => {
+    const booking = req.body;
+    console.log(booking);
+    const result = await bookingCollection.insertOne(booking);
+    res.send(result);
+  });
+
+
+  // booking update
+   app.patch('/bookings/:id', async(req, res) =>{
+    const id = req.params.id;
+    const filter =  {_id: new ObjectId(id)}; 
+    const updatedBooking = req.body;
+    console.log(updatedBooking);
+    const updateDoc = {
+        $set: {
+            status: updatedBooking.status
+        }
+    }
+    const result = await bookingCollection.updateOne(filter, updateDoc);
+    res.send(result);
+   })
+
+
+
+  //booking delete
+  app.delete('/bookings/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}; 
+    const result = await bookingCollection.deleteOne(query);
+    res.send(result);
+  })
+
+
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
