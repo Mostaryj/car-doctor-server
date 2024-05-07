a3d0c8dfb74cba5d957b7264876d2ec4de686fdd
Mostary Jahan <mostaryjahan01@gmail.com>
2024-05-07 16:25:52 +0600

    done

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 
 
 //middle ware
-app.use(cors());
+app.use(cors({
+    origin: [
+     'http://localhost:5173',
+    'https://cars-doctor-2bb00.web.app',
+    'https://cars-doctor-2bb00.firebaseapp.com',
+    
+    ],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 console.log(process.env.DB_PASS);
 
@@ -25,6 +36,42 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+//middlewares
+const logger = async(req, res, next) =>{
+    console.log('logger info:', req.hostname, req.originalUrl)
+    next();
+}
+
+const verifyToken = async(req, res, next) =>{
+    const token = req.cookies?.token;
+    console.log('value of token from middlewares', token)
+    if(!token){
+        return res.status(401).send({message: 'not authorized'})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        //error
+          if(err){
+             console.log(err);
+            return res.status(401).send({message: 'unauthorized'})
+          }
+
+        //if token is valid then it would be decoded
+        console.log('value in the token', decoded)
+        req.user = decoded;
+        next();
+    })
+   
+ }
+
+ const cookieOption = {
+  httpOnly: true,
+  secure:process.env.NODE_ENV === 'production'? true : false,
+  sameSite: process.env.NODE_ENV === 'production'? 'none' : 'strict'
+
+}
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -33,7 +80,33 @@ async function run() {
    const serviceCollection = client.db('carDoctor').collection('services');
    const bookingCollection = client.db('carDoctor').collection('booking');
 
-   app.get('/services', async(req, res) =>{
+
+
+//auth related api
+app.post('/jwt', logger, async(req,res) => {
+    const user = req.body;
+    console.log('user for token',user);
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'})
+
+    res
+    .cookie('token', token, cookieOption)
+    .send({success: true});
+})
+
+
+
+//log out
+app.post('/logout', async(req, res) => {
+    const user = req.body;
+    console.log(10,'logging out',user);
+    res.clearCookie('token from logOut',{...cookieOption, maxAge: 0}).send({success: true});
+})
+
+
+
+
+   //services related api
+   app.get('/services', logger,  async(req, res) =>{
      const cursor = serviceCollection.find();
      const result = await cursor.toArray();
      res.send(result);
@@ -52,22 +125,30 @@ async function run() {
   })
 
 
+
+
   //booking
-  
-  app.get('/bookings', async(req, res) =>{
-    console.log(req.query.email);
-    let query ={};
-    if(req.query?.email){
-        query= {email: req.query.email}
+  app.get('/bookings-email/:email', logger,verifyToken, async(req, res) =>{
+    console.log('user in the valid token',req.params.email);
+     console.log('token owner info', req.user)
+
+    if(req.params.email !== req.user.email){
+          return res.status(403).send({message: 'forbidden access'})
     }
-    const cursor = bookingCollection.find();
-    const result = await cursor.toArray();
+    let query ={};
+    if(req.params?.email){
+        query= {email: req.params.email}
+    }
+    const result = await bookingCollection.find(query).toArray();
     res.send(result);
   })
 
+
+
+
   app.post('/bookings', async(req, res) => {
     const booking = req.body;
-    console.log(booking);
+    console.log(3,booking);
     const result = await bookingCollection.insertOne(booking);
     res.send(result);
   });
@@ -78,7 +159,7 @@ async function run() {
     const id = req.params.id;
     const filter =  {_id: new ObjectId(id)}; 
     const updatedBooking = req.body;
-    console.log(updatedBooking);
+    console.log(4,updatedBooking);
     const updateDoc = {
         $set: {
             status: updatedBooking.status
